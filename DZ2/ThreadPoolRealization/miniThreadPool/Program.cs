43c21cabc97cf0708eab14b31dc6140c29227766
Alexander Fakhrudinov <asbuka@gmail.com>
2021-10-19 08:37:53 +0300

    DZ2_threadPool

---
+++ DZ2/ThreadPoolRealization/miniThreadPool/Program.cs
@@ -0,0 +1,24 @@
+﻿using MiniThreadPool.MyAction;
+using MiniThreadPool.ThreadPool;
+using System;
+
+namespace MiniThreadPool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("miniThreadPool");
+
+            MyThreadPool myTreadPool = new MyThreadPool(4);
+
+            Console.WriteLine("Threads count from main thread = " + myTreadPool.GetCountOfThreads());
+            for (int i = 0; i < 15; i++)
+            {
+                myTreadPool.Enqueue(new MyTask(i));
+            }
+            Console.ReadKey();
+            myTreadPool.WaitForJoinThread();    
+        }
+    }
+}
