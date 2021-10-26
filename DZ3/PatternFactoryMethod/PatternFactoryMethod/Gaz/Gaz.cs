baf34ab98b74b51e9658cfee4808f864a044769b
Alexander Fakhrudinov <asbuka@gmail.com>
2021-10-26 20:01:46 +0300

    dz3

---
+++ DZ3/PatternFactoryMethod/PatternFactoryMethod/Gaz/Gaz.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PatternFactoryMethod.Gaz
+{
+    public class Gaz : BaseClassForCar
+    {
+        public string SerialNumber { get; private set; }
+
+        public Gaz(string model) : base("Газ", model.ToString())
+        {
+            // Создаем генератор случайных чисел для серийного номера
+            Random rnd = new Random();
+            SerialNumber = rnd.Next(1000000, 9999999).ToString() + "GAZ";
+        }
+
+        public override string ToString()
+        {
+            return $"машина {base.Model}, произведена заводом {FactoryName}, с номером {SerialNumber}";
+        }
+    }
+}
---
+++ DZ3/PatternFactoryMethod/PatternFactoryMethod/Vaz/Vaz.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PatternFactoryMethod.Vaz
+{
+    public class Vaz : BaseClassForCar
+    {
+        public string SerialNumber { get; private set; }
+
+        public Vaz(string model) : base("Ваз", model)
+        {
+            // Создаем генератор случайных чисел для серийного номера
+            Random rnd = new Random();
+            SerialNumber = "vz" + rnd.Next(1000000, 9999999).ToString();
+        }
+
+        public override string ToString()
+        {
+            return $"машина {base.Model}, произведена заводом {FactoryName}, с номером {SerialNumber}";
+        }
+    }
+}
